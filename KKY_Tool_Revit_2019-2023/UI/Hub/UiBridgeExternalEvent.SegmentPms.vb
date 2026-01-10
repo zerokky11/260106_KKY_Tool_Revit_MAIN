eb3fc6b24e2e19c44b2260195a5df3e8ff5c2f11
zerokky11 <kkykiki89@nate.com>
2026-01-10 16:43:53 +0900

    Consolidate UiBridgeExternalEvent helpers

--- KKY_Tool_Revit_2019-2023/UI/Hub/UiBridgeExternalEvent.SegmentPms.vb
+++ KKY_Tool_Revit_2019-2023/UI/Hub/UiBridgeExternalEvent.SegmentPms.vb
@@ -836,6 +836,24 @@ Namespace UI.Hub
             End Try
         End Function
 
+        Private Shared Function GetDictValue(dict As Dictionary(Of String, Object), key As String) As Object
+            If dict Is Nothing OrElse String.IsNullOrWhiteSpace(key) Then
+                Return Nothing
+            End If
+            Dim v As Object = Nothing
+            If dict.TryGetValue(key, v) Then
+                Return v
+            End If
+            Return Nothing
+        End Function
+
+        Private Shared Function SafeStr(o As Object) As String
+            If o Is Nothing OrElse o Is DBNull.Value Then
+                Return String.Empty
+            End If
+            Return Convert.ToString(o)
+        End Function
+
         Private Shared Function SegPmsSafeStr(o As Object) As String
             Return SafeStr(o)
         End Function
